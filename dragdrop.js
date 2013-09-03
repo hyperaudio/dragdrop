216335d451b5fd7414df7144dc2f46fc16b67e85
cubiq <matteo@cubiq.org>
2013-09-03 12:11:31 +0200

    sometimes I hate git

---
+++ dragdrop.js
@@ -0,0 +1,213 @@
+var DragDrop = (function (window, document) {
+
+	function DragDrop (handle, droppable, options) {
+		this.options = {
+			touch: true,
+			mouse: true,
+			timeout: 500,
+			html: ''
+		};
+
+		for ( var i in options ) {
+			this.options[i] = options[i];
+		}
+
+		this.handle = typeof handle == 'string' ? document.querySelector(handle) : handle;
+		this.droppable = typeof droppable == 'string' ? document.querySelector(droppable) : droppable;
+
+		// Create the list and the placeholder
+		this.list = this.droppable.querySelector('ul');
+		if ( !this.list ) {
+			this.list = document.createElement('ul');
+			this.droppable.appendChild(this.list);
+		}
+		this.placeholder = document.createElement('li');
+		this.placeholder.className = 'placeholder';
+
+		// Are we reordering the list?
+		this.reordering = this.handle.parentNode == this.list;
+
+		if ( this.options.touch ) {
+			this.handle.addEventListener('touchstart', this, false);
+		}
+
+		if ( this.options.mouse ) {
+			this.handle.addEventListener('mousedown', this, false);
+		}
+	}
+
+	DragDrop.prototype.handleEvent = function (e) {
+		switch (e.type) {
+			case 'mousedown':
+				if ( e.which !== 1 ) {
+					break;
+				}
+			case 'touchstart':
+				this.start(e);
+				break;
+			case 'touchmove':
+			case 'mousemove':
+				this.move(e);
+				break;
+			case 'touchend':
+			case 'mouseup':
+				this.end(e);
+				break;
+		}
+	};
+
+	DragDrop.prototype.start = function (e) {
+		e.preventDefault();
+
+		clearTimeout(this.dragTimeout);
+		this.initiated = false;
+		this.lastTarget = null;
+
+		if ( this.options.touch ) {
+			document.addEventListener('touchend', this, false);
+		}
+
+		if ( this.options.mouse ) {
+			document.addEventListener('mouseup', this, false);
+		}
+
+		this.dragTimeout = setTimeout(this.init.bind(this, this.options.html, e), this.options.timeout);
+	};
+
+	DragDrop.prototype.init = function (html, e) {
+		// Create draggable
+		this.draggable = document.createElement('div');
+		this.draggable.className = 'draggable';
+		this.draggableStyle = this.draggable.style;
+		this.draggableStyle.cssText = 'position:absolute;z-index:1000;pointer-events:none;left:-99999px';
+		this.draggable.innerHTML = html;
+
+		document.body.appendChild(this.draggable);
+
+		this.draggableCenterX = Math.round(this.draggable.offsetWidth / 2);
+		this.draggableCenterY = Math.round(this.draggable.offsetHeight / 2);
+
+		this.position(e);
+
+		if ( this.options.touch ) {
+			document.addEventListener('touchmove', this, false);
+		}
+
+		if ( this.options.mouse ) {
+			document.addEventListener('mousemove', this, false);
+		}
+
+		this.initiated = true;
+
+		// If we are reordering the list, hide the current element
+		if ( this.reordering ) {
+			this.handle.style.display = 'none';
+		}
+
+		this.move(e);
+
+		if ( this.options.onDragStart ) {
+			this.options.onDragStart.call(this);
+		}
+	};
+
+	DragDrop.prototype.position = function (e) {
+		var point = e.changedTouches ? e.changedTouches[0] : e;
+
+		this.draggableStyle.left = point.pageX - this.draggableCenterX + 'px';
+		this.draggableStyle.top = point.pageY - this.draggableCenterY + 'px';
+	};
+
+	DragDrop.prototype.move = function (e) {
+		e.preventDefault();
+
+		var point = e.changedTouches ? e.changedTouches[0] : e;
+		var target = e.touches ? document.elementFromPoint(point.pageX, point.pageY) : point.target;
+
+		this.position(e);
+
+		if ( target == this.lastTarget || target == this.placeholder || target == this.list ) {
+			return;
+		}
+
+		this.lastTarget = target;
+
+		if ( this.list.querySelector('.placeholder') ) {
+			this.list.removeChild(this.placeholder);
+		}
+
+		if ( target == this.droppable ) {
+			this.list.appendChild(this.placeholder);
+			return;
+		}
+
+		if ( target.className == 'item' ) {
+			var items = this.list.querySelectorAll('.item'),
+				i = 0, l = items.length;
+			for ( ; i < l; i++ ) {
+				if ( target == items[i] ) {
+					this.list.insertBefore(this.placeholder, items[i]);
+					break;
+				}
+			}
+		}
+	};
+
+	DragDrop.prototype.end = function (e) {
+		clearTimeout(this.dragTimeout);
+
+		document.removeEventListener('touchend', this, false);
+		document.removeEventListener('mouseup', this, false);
+
+		if ( !this.initiated ) {
+			return;
+		}
+
+		document.removeEventListener('touchmove', this, false);
+		document.removeEventListener('mousemove', this, false);
+
+		var point = e.changedTouches ? e.changedTouches[0] : e;
+		var target = e.touches ? document.elementFromPoint(point.pageX, point.pageY) : point.target;
+
+		var html = this.draggable.innerHTML;
+		this.draggable.parentNode.removeChild(this.draggable);
+		this.draggable = null;
+
+		// if we are reordering, remove the original element
+		if ( this.reordering ) {
+			if ( this.handle._dragInstance ) {
+				this.handle._dragInstance.destroy();
+				this.handle._dragInstance = null;
+			}
+
+			this.handle.parentNode.removeChild(this.handle);
+		}
+
+		// we dropped outside of the draggable area, so exit
+		if ( !this.list.querySelector('.placeholder') ) {
+			return;
+		}
+
+		var el = document.createElement('li');
+		el.className = 'item';
+		el.innerHTML = html;
+
+		this.list.insertBefore(el, this.placeholder);
+		this.placeholder.parentNode.removeChild(this.placeholder);
+
+		if ( this.options.onDrop ) {
+			this.options.onDrop.call(this, el);
+		}
+	};
+
+	DragDrop.prototype.destroy = function () {
+		document.removeEventListener('touchstart', this, false);
+		document.removeEventListener('touchmove', this, false);
+		document.removeEventListener('touchend', this, false);
+		document.removeEventListener('mousedown', this, false);
+		document.removeEventListener('mousemove', this, false);
+		document.removeEventListener('mouseup', this, false);
+	}
+
+	return DragDrop;
+})(window, document);
